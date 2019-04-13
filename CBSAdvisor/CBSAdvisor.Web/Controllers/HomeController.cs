8c793376d5db1680b0cb676d8b11bd964f2951e4
CBSAdvisor <cbs.advisor@gmail.com>
2019-04-13 16:24:15 +0300

    Change Home view message

--- CBSAdvisor/CBSAdvisor.Web/Controllers/HomeController.cs
+++ CBSAdvisor/CBSAdvisor.Web/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace CBSAdvisor.Web.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "CBSAdvisor Web application.";
 
             return View();
         }
